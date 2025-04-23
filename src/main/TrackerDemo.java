945cd38be151bbd081bbd7ab82a538d9315cc15b
Prachi Jindal <prachibansal135@gmail.com>
2025-04-23 12:07:43 +0200

    Added fields item and expiration date to Perishable and used it in Tracker Demo with milk as an instance

--- src/main/TrackerDemo.java
+++ src/main/TrackerDemo.java
@@ -2,9 +2,12 @@ package main;
 
 import model.Book;
 import model.Device;
+import model.Perishable;
 import model.Snack;
 import storage.StorageUnit;
 import storage.StorageUtils;
+
+import java.time.LocalDate;
 
 public class TrackerDemo {
 
@@ -13,14 +16,19 @@ public class TrackerDemo {
         StorageUnit<Book> book1 = new StorageUnit<Book>();
         StorageUnit<Device> device1 = new StorageUnit<Device>();
         StorageUnit<Snack> snack1 = new StorageUnit<Snack>();
+        StorageUnit<Perishable> perishable1 = new StorageUnit<>();
 
         book1.addItem(new Book("Harry Potter"));
         device1.addItem(new Device("Iphone16Pro"));
         snack1.addItem(new Snack("Pizza"));
+        perishable1.addItem(new Perishable("Milk", LocalDate.of(2025, 7, 25)));
+
 
         StorageUtils.printStorageUnitItems(book1);
         StorageUtils.printStorageUnitItems(device1);
         StorageUtils.printStorageUnitItems(snack1);
+        StorageUtils.printStorageUnitItems(perishable1);
+
 
 
 
--- src/model/Perishable.java
+++ src/model/Perishable.java
@@ -1,10 +1,30 @@
 package model;
 
-public class Perishable {
-    private int expirationDate;
+import java.time.LocalDate;
 
-    public Perishable(int expirationDate) {
+public class Perishable {
+    private String nameOfPerishable;
+    private LocalDate expirationDate;
+
+    public Perishable(String nameOfPerishable, LocalDate expirationDate) {
+        this.nameOfPerishable = nameOfPerishable;
         this.expirationDate = expirationDate;
+    }
+
+    public boolean isExpired() {
+        return expirationDate.isBefore(LocalDate.now());
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Perishable{" +
+                "nameOfPerishable='" + nameOfPerishable + '\'' +
+                ", expirationDate=" + expirationDate +
+                '}';
     }
 
 

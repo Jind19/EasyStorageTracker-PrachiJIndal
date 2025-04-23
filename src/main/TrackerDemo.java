3cb1c077fdb95b51d1bf42c69b7f1e4a8a7d777d
Prachi Jindal <prachibansal135@gmail.com>
2025-04-23 11:51:04 +0200

    Added one book, one device and one snack in StorageUnit and print the items in StorageUnit using Storage Utils

--- src/main/TrackerDemo.java
+++ src/main/TrackerDemo.java
@@ -1,4 +1,29 @@
 package main;
 
+import model.Book;
+import model.Device;
+import model.Snack;
+import storage.StorageUnit;
+import storage.StorageUtils;
+
 public class TrackerDemo {
+
+    public static void main(String[] args) {
+
+        StorageUnit<Book> book1 = new StorageUnit<Book>();
+        StorageUnit<Device> device1 = new StorageUnit<Device>();
+        StorageUnit<Snack> snack1 = new StorageUnit<Snack>();
+
+        book1.addItem(new Book("Harry Potter"));
+        device1.addItem(new Device("Iphone16Pro"));
+        snack1.addItem(new Snack("Pizza"));
+
+        StorageUtils.printStorageUnitItems(book1);
+        StorageUtils.printStorageUnitItems(device1);
+        StorageUtils.printStorageUnitItems(snack1);
+
+
+
+
+    }
 }
--- src/storage/StorageUtils.java
+++ src/storage/StorageUtils.java
@@ -1,4 +1,8 @@
 package storage;
 
 public class StorageUtils {
+
+    public static void printStorageUnitItems(StorageUnit<? extends Object> item){
+        System.out.println("Storage Unit contains: " + item.getItem());
+    }
 }
